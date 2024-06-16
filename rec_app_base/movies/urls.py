f3260fb75ee26f553cc56d3dabb012b14d9b2df4
Yun Xi Chen <cadeychen@MacBook-Pro-3.local>
2024-06-16 13:36:48 -0400

    Fixed image url not found error

--- rec_app_base/movies/urls.py
+++ rec_app_base/movies/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path("about/", views.about, name="movies-about")
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
