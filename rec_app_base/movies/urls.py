91ce1376e869eb7f063e1de43e7b11d62c7cb70f
Cadey Chen <cadeychen7@gmail.com>
2024-07-04 22:02:41 -0400

    Added basic bootstrap HTML templates for UI

--- rec_app_base/movies/urls.py
+++ rec_app_base/movies/urls.py
@@ -4,8 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("", views.home, name="movies_home"),
-    path("about/", views.about, name="movies-about")
+    path("", views.home, name="home"),
+    path("about/", views.about, name="movies-about"),
+    path("movies_by_genre/", views.movies_by_genre_view, name="movies-by-genre"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
